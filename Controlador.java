9669b16c13ac44e2ad2c52f96cea739100487c56
Nenerox <andres.ps0308@gmail.com>
2025-11-03 11:41:54 -0600

    Se crearon los atributos del usuario y se inicio el controlador

--- Controlador.java
+++ Controlador.java
@@ -1,3 +1,23 @@
+
+import java.util.ArrayList;
+
 public class Controlador {
-    
+    private ArrayList <Usuario> usuarios = new ArrayList<>();
+
+    public Controlador() {
+        //usuarios por defecto
+        usuarios.add(new Usuario("admin", "admin123", "administrador"));
+        usuarios.add(new Usuario("editor", "editor123", "editor"));
+    }
+
+    public String autenticar(String username, String contraseña) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.verificacion(username, contraseña)) {
+                return "Bienvenido " + usuario.getUsername() + ", tu rol es: " + usuario.getRol();
+            }
+        }
+        return "Credenciales incorrectas";
+    }
+
+
 }
---
+++ Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+    private String username;
+    private String contraseña;
+    private String rol;
+
+    public Usuario(String username, String contraseña, String rol) {
+        this.username = username;
+        this.contraseña = contraseña;
+    }
+
+    public boolean verificacion(String usaname, String contraseña) {
+        return this.username.equals(username) && this.contraseña.equals(contraseña);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+}
